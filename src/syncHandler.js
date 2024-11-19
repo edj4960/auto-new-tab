7d60880524bb88ba84d8bf664c19a9640285554a
edj4960 <evandj423@gmail.com>
2024-11-19 14:51:00 -0700

    Code formatting for syncHandler

--- src/syncHandler.js
+++ src/syncHandler.js
@@ -1,25 +1,25 @@
 const SyncHandler = {
-    get(key) {
-        return new Promise(resolve => {
-            chrome.storage.sync.get([key], result => {
-                resolve(result[key]);
-            });
-        });
-    },
+  get(key) {
+    return new Promise(resolve => {
+      chrome.storage.sync.get([key], result => {
+        resolve(result[key]);
+      });
+    });
+  },
 
-    set(key, value) {
-        return new Promise(resolve => {
-            chrome.storage.sync.set({ [key]: value }, () => {
-                resolve();
-            });
-        });
-    },
+  set(key, value) {
+    return new Promise(resolve => {
+      chrome.storage.sync.set({ [key]: value }, () => {
+        resolve();
+      });
+    });
+  },
 
-    onChange(callback) {
-        chrome.storage.onChanged.addListener((changes, areaName) => {
-            if (areaName === 'sync') {
-                callback(changes);
-            }
-        });
-    },
+  onChange(callback) {
+    chrome.storage.onChanged.addListener((changes, areaName) => {
+      if (areaName === 'sync') {
+        callback(changes);
+      }
+    });
+  },
 };
